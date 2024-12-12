a51872ed1e7411ab820d4242db51fd6d01b37cbf
Jona20Tan <jona@salad.ventures>
2024-12-12 10:48:04 +0800

    [WIP] homepage fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Container } from './layout/Container';
+import React from 'react';
 
 export function Hero() {
   const settings = {
@@ -61,7 +62,7 @@ export function Hero() {
                 {createArcText()}
               </div>
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-[-10%]">
-                <div className="w-64 h-64 md:w-68 md:h-68 animate-bounce flex items-center justify-center">
+                <div className="w-60 h-60 md:w-96 md:h-96 animate-bounce flex items-center justify-center">
                   {/* <Sparkles className="w-24 h-24 md:w-32 md:h-32 text-purple-600" /> */}
                   <img src="/airpark-logo.png"></img>
                 </div>
